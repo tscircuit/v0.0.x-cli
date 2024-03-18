f260d6eefcf5b6f05186a715af2b1454b5a9c7de
seveibar <seveibar@gmail.com>
2024-03-17 19:52:57 -0700

    logout and config clear properly implemented

--- lib/cmd-fns/auth-logout.ts
+++ lib/cmd-fns/auth-logout.ts
@@ -2,5 +2,6 @@ import kleur from "kleur"
 import { AppContext } from "../util/app-context"
 
 export const authLogout = async (ctx: AppContext, args: any) => {
-  // console.log(kleur.green("Logged out!"))
+  ctx.profile_config.delete("session_token")
+  console.log(kleur.green("Logged out!"))
 }
--- lib/cmd-fns/config-clear.ts
+++ lib/cmd-fns/config-clear.ts
@@ -1,4 +1,3 @@
-import { unlinkSync } from "fs"
 import { AppContext } from "../util/app-context"
 
 export const configClear = async (ctx: AppContext, args: any) => {
