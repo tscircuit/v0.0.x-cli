a3f11ad64ee7af58f972e886d3d8f468250969d3
seveibar <seveibar@gmail.com>
2024-03-19 07:01:18 -0700

    new way of deriving tscircuit version

--- lib/cmd-fns/version.ts
+++ lib/cmd-fns/version.ts
@@ -3,25 +3,21 @@ import cliPackageJson from "./../../package.json"
 import frontendPackageJson from "dev-server-frontend/package.json"
 
 export const versionCmd = async (ctx: AppContext, args: any) => {
-  const tscircuitPackageJson = await import(
-    "tscircuit/package.json" as any
-  ).catch((e) => ({ version: "" }))
-  const reactFiberPackageJson = await import(
-    "@tscircuit/react-fiber/package.json"
-  ).catch((e) => ({ version: "" }))
-  const builderPackageJson = await import(
-    "@tscircuit/builder/package.json"
-  ).catch((e) => ({ version: "" }))
-  if (tscircuitPackageJson.version) {
-    console.log(`tscircuit@${cliPackageJson.version}`)
+  if (typeof global !== "undefined" && (global as any).TSCIRCUIT_VERSION) {
+    console.log(`tscircuit@${(global as any).TSCIRCUIT_VERSION}`)
+    console.log("")
   }
   console.log(`@tscircuit/cli@${cliPackageJson.version}`)
-  console.log(`@tscircuit/react-fiber@${reactFiberPackageJson.version}`)
+  console.log(
+    `@tscircuit/react-fiber@${cliPackageJson.dependencies["@tscircuit/react-fiber"]}`
+  )
   console.log(
     `@tscircuit/schematic-viewer@${frontendPackageJson.dependencies["@tscircuit/schematic-viewer"]}`
   )
   console.log(
     `@tscircuit/pcb-viewer@${frontendPackageJson.dependencies["@tscircuit/pcb-viewer"]}`
   )
-  console.log(`@tscircuit/builder@${builderPackageJson.version}`)
+  console.log(
+    `@tscircuit/builder@${cliPackageJson.dependencies["@tscircuit/builder"]}`
+  )
 }
