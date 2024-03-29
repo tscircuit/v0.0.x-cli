2b99822677ade1b410d15ee209eb3ba029cb3ed8
seveibar <seveibar@gmail.com>
2024-03-29 09:44:00 -0700

    cleanup

--- lib/get-program.ts
+++ lib/get-program.ts
@@ -3,29 +3,10 @@ import packageJson from "../package.json"
 import { AppContext } from "./util/app-context"
 import * as CMDFN from "lib/cmd-fns"
 
-// | Endpoint                              | Description                  |
-// | ------------------------------------- | ---------------------------- |
-// | /packages/search         | Search for packages          |
-// | /packages/list           | List packages with a filter  |
-// | /packages/get            | Get a package by id or name  |
-// | /packages/create         | Create a new package         |
-// | /package_releases/list   | List package releases        |
-// | /package_releases/get    | Get a package release by id  |
-// | /package_releases/create | Create a new package release |
-// | /package_files/list      | List package files           |
-// | /package_files/get       | Get a package file by id     |
-// | /package_files/download  | Download a package file      |
-// | /package_files/create    | Create a new package file    |
-// | /package_examples/list   | List package examples        |
-// | /package_examples/get    | Get a package example by id  |
-// | /package_examples/create | Create a new package example |
-
 export const getProgram = (ctx: AppContext) => {
   const cmd = new Command("tsci").description(
     "Develop, test and manage tscircuit packages"
   )
-
-  cmd
 
   const authCmd = cmd.command("auth").description("Login/logout")
   authCmd
