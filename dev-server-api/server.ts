9092d3ac32235f5e8da44fd253f363b5497c20a2
Severin Ibarluzea <seveibar@gmail.com>
2024-07-03 23:17:03 -0700

    Change Start Script for api server to use bun (#61)
    * change start script but broke api server (now having json errors)
    
    * upgrade winterspec, fixing the compat serving issue
    
    * update bun lockfiles

--- dev-server-api/server.ts
+++ dev-server-api/server.ts
@@ -1,9 +1,17 @@
 import { createFetchHandlerFromDir } from "winterspec/adapters/node"
+import { Request as EdgeRuntimeRequest } from "@edge-runtime/primitives"
 
 const serverFetch = await createFetchHandlerFromDir("./routes")
 
 console.log("starting dev-server-api on localhost:3021")
 Bun.serve({
-  fetch: (req) => serverFetch(req),
+  fetch: (bunReq) => {
+    const req = new EdgeRuntimeRequest(bunReq.url, {
+      headers: bunReq.headers,
+      method: bunReq.method,
+      body: bunReq.body,
+    })
+    return serverFetch(req as any)
+  },
   port: 3021,
 })
