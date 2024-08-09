1dd23e8953b742fc805a1fea0e69fea297bca0f5
seveibar <seveibar@gmail.com>
2024-08-09 16:09:35 -0700

    improve open test

--- tests/open.test.ts
+++ tests/open.test.ts
@@ -1,7 +1,8 @@
 import { test, expect } from "bun:test"
 import { $ } from "bun"
 
-test("tsci open", async () => {
+// This test annoyingly opens a browser window, unskip if you're testing it
+test.skip("tsci open", async () => {
   const result = await $`bun cli.ts open -y --cwd ./example-project`.text()
   expect(result).toContain("http")
   expect(result).toContain("example-project")
