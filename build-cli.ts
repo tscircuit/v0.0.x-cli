f9e5c271e80e13e05ed268620c422872ae07f3c6
seveibar <seveibar@gmail.com>
2024-08-09 15:59:25 -0700

    fix build issue with cli packages

--- build-cli.ts
+++ build-cli.ts
@@ -1,13 +1,11 @@
 import mainPkg from "./package.json"
-import apiPkg from "./dev-server-api/package.json"
 
 await Bun.build({
   entrypoints: ["./cli.ts"],
   external: [
     ...Object.keys(mainPkg.dependencies),
     ...Object.keys(mainPkg.devDependencies),
-    ...Object.keys(apiPkg.dependencies),
-    ...Object.keys(apiPkg.devDependencies),
+    ...Object.keys(mainPkg.peerDependencies),
   ],
   outdir: "dist",
   target: "node",
