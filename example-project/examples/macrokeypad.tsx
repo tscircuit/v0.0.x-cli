c57e10f206a014399ef9d6c0693f56803c5b709e
seveibar <seveibar@gmail.com>
2024-09-06 15:54:50 -0700

    9key macrokeypad part 2

--- example-project/examples/macrokeypad.tsx
+++ example-project/examples/macrokeypad.tsx
@@ -1,10 +1,55 @@
 import { layout } from "@tscircuit/layout"
+import { ArduinoProMicroBreakout } from "example-project/src/ArduinoProMicroBreakout"
+import { Key } from "example-project/src/Key"
 import manualEdits from "example-project/src/manual-edits"
 import { SwitchShaft } from "example-project/src/SwitchShaft"
 
-export const MacroKeypad = () => (
-  <board width="20mm" height="20mm">
-    <SwitchShaft name="SW1" pcbX={3} pcbY={3} />
-    {/* <SwitchShaft name="SW2" /> */}
-  </board>
-)
+export const MacroKeypad = () => {
+  const keyPositions = Array.from({ length: 9 })
+    .map((_, i) => ({
+      keyNum: i + 1,
+      col: i % 3,
+      row: Math.floor(i / 3),
+    }))
+    .map((p) => ({
+      ...p,
+      x: p.col * 19.05 - 19.05,
+      y: p.row * 19.05 - 19.05,
+    }))
+
+  const rowToMicroPin = {
+    0: "D2",
+    1: "D3",
+    2: "D4",
+  }
+  const colToMicroPin = {
+    0: "D5",
+    1: "D6",
+    2: "D7",
+  }
+
+  return (
+    <board width="120mm" height="80mm">
+      {keyPositions.map(({ keyNum, x, y }) => (
+        <Key name={`K${keyNum}`} keyNum={keyNum} pcbX={x} pcbY={y} />
+      ))}
+      <ArduinoProMicroBreakout key="u1" name="U1" pcbX={44} />
+      {keyPositions.map(({ keyNum, row, col }) => (
+        <trace
+          // @ts-ignore
+          key={`trace-${keyNum}-col`}
+          from={`.SW${keyNum} .pin1`}
+          to={`.U1 .${colToMicroPin[col as 0 | 1 | 2]}`}
+        />
+      ))}
+      {keyPositions.map(({ keyNum, row, col }) => (
+        <trace
+          // @ts-ignore
+          key={`trace-${keyNum}-row`}
+          from={`.D${keyNum} .pin2`}
+          to={`.U1 .${rowToMicroPin[row as 0 | 1 | 2]}`}
+        />
+      ))}
+    </board>
+  )
+}
--- example-project/src/SwitchShaft.tsx
+++ example-project/src/SwitchShaft.tsx
@@ -14,30 +14,23 @@ export const SwitchShaft = (props: {
     {...props}
     footprint={
       <footprint>
+        {/* <silkscreentext text={props.name} /> */}
         <smtpad
           shape="rect"
           width="2.55mm"
           height="2.5mm"
           portHints={["pin1"]}
+          layer="top"
         />
         <smtpad
           shape="rect"
           width="2.55mm"
           height="2.5mm"
           portHints={["pin2"]}
+          layer="top"
         />
-        <platedhole
-          shape="circle"
-          name="H1"
-          holeDiameter="3mm"
-          outerDiameter="3.1mm"
-        />
-        <platedhole
-          shape="circle"
-          name="H2"
-          holeDiameter="3mm"
-          outerDiameter="3.1mm"
-        />
+        <hole name="H1" diameter="3mm" />
+        <hole name="H2" diameter="3mm" />
         <constraint xDist="6.35mm" centerToCenter left=".H1" right=".H2" />
         <constraint yDist="2.54mm" centerToCenter top=".H1" bottom=".H2" />
         <constraint edgeToEdge xDist="11.3mm" left=".pin1" right=".pin2" />
