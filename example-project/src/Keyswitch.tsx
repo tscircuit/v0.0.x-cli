294d196411ce2586766943caa2555dc30facfc50
seveibar <seveibar@gmail.com>
2024-09-23 15:48:54 -0700

    keyboard mostly working

---
+++ example-project/src/Keyswitch.tsx
@@ -0,0 +1,33 @@
+/**
+ * Keyswitch (Brown)
+ *
+ * https://www.lcsc.com/datasheet/lcsc_datasheet_1912111437_Kailh-CPG1511F01S03_C400227.pdf
+ */
+export const Keyswitch = (props: {
+  name: string
+  pcbX?: number
+  pcbY?: number
+}) => {
+  return (
+    <chip
+      {...props}
+      cadModel={{
+        objUrl:
+          "https://modelcdn.tscircuit.com/easyeda_models/download?pn=C400227",
+        rotationOffset: { x: 180, y: 0, z: -90 },
+      }}
+      footprint={
+        <footprint>
+          <smtpad
+            shape="rect"
+            width="0.1mm"
+            height="0.1mm"
+            portHints={["pin1"]}
+            layer="top"
+          />
+          <hole diameter={2.5} pcbX={0} pcbY={0} />
+        </footprint>
+      }
+    />
+  )
+}
